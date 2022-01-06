7959d8abde73d0365dc657ff5ef78185322dd115
HPHAN1 <huyphan@rocketmortgage.com>
2022-01-05 22:22:15 -0500

    added sql file

---
+++ CarDealershipAPISQL.sql
@@ -0,0 +1,17 @@
+-- create database CarAPI;
+-- create table cars(
+-- ID int not null auto_increment primary key,
+-- make nvarchar(30) not null,
+-- model nvarchar(30) not null,
+-- `year` int,
+-- color nvarchar(10) not null
+-- );
+
+-- insert into cars
+-- values(0,'Jeep','Wrangler',2020,'Green'),
+-- (0,'Dodge','Grand Caravan',2019,'Silver'),
+-- (0,'Ford','Mustang',2015,'Red'),
+-- (0,'Ford','Bronco',2012,'Yellow'),
+-- (0,'Catillac','Escalade',2018,'White');
+
+select * from cars;
